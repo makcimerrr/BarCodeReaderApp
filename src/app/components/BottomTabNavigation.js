91ac0083fc313cb1dc1abd1cac80295094096712
makcimerrr <maximedubs@proton.me>
2024-10-12 19:51:41 +0200

    Add animation to BottomTabNavigation component
    Introduced opacity animations to the Home and Search tabs using React Native's Animated API. This enhances user experience by providing visual feedback on tab changes. Implemented useRef to hold the animation value and applied Animated.timing for the transitions.

--- src/app/components/BottomTabNavigation.js
+++ src/app/components/BottomTabNavigation.js
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {View, TouchableOpacity, Text, StyleSheet, Animated} from 'react-native';
 import {useNavigation, useNavigationState} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome'; // Importer FontAwesome
 
 const BottomTabNavigation = () => {
     const navigation = useNavigation();
     const [activeTab, setActiveTab] = useState('Home');
+    const opacity = useRef(new Animated.Value(1)).current; // Valeur d'animation
 
     // Utilisation de useNavigationState pour obtenir la route active
     const navigationState = useNavigationState((state) => state);
@@ -14,40 +15,46 @@ const BottomTabNavigation = () => {
         // Récupérer la route active
         const currentRoute = navigationState?.routes[navigationState.index]?.name;
         setActiveTab(currentRoute);
+        // Lancer l'animation
+        Animated.sequence([
+            Animated.timing(opacity, {
+                toValue: 0, // Disparaît
+                duration: 250, // Durée de l'animation en millisecondes
+                useNativeDriver: true,
+            }),
+            Animated.timing(opacity, {
+                toValue: 1, // Réapparaît
+                duration: 250,
+                useNativeDriver: true,
+            })
+        ]).start();
     }, [navigationState]);
 
     return (
         <View style={styles.container}>
-            {/*<TouchableOpacity*/}
-            {/*    onPress={() => navigation.navigate('Explore')}*/}
-            {/*    style={[styles.tab, activeTab === 'Explore' && styles.activeTab]} // Appliquer le style actif si c'est le cas*/}
-            {/*>*/}
-            {/*    <View style={[styles.iconContainer, activeTab === 'Explore' && styles.activeIconContainer]}>*/}
-            {/*        <Icon name="location-arrow" size={24} color={activeTab === 'Explore' ? '#fff' : '#fff'}/>*/}
-            {/*    </View>*/}
-            {/*    <Text style={[styles.label, activeTab === 'Explore' && styles.activeLabel]}>Explore</Text>*/}
-            {/*</TouchableOpacity>*/}
-            <TouchableOpacity
+            <AnimatedTouchableOpacity
                 onPress={() => navigation.navigate('Home')}
-                style={[styles.tab, activeTab === 'Home' && styles.activeTab]} // Appliquer le style actif si c'est le cas
+                style={[styles.tab, activeTab === 'Home' && styles.activeTab, {opacity}]} // Appliquer le style actif si c'est le cas
             >
                 <View style={[styles.iconContainer, activeTab === 'Home' && styles.activeIconContainer]}>
                     <Icon name="home" size={24} color={activeTab === 'Home' ? '#fff' : '#fff'}/>
                 </View>
                 <Text style={[styles.label, activeTab === 'Home' && styles.activeLabel]}>Home</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
+            </AnimatedTouchableOpacity>
+            <AnimatedTouchableOpacity
                 onPress={() => navigation.navigate('Search')}
-                style={[styles.tab, activeTab === 'Search' && styles.activeTab]} // Appliquer le style actif si c'est le cas
+                style={[styles.tab, activeTab === 'Search' && styles.activeTab, {opacity}]} // Appliquer le style actif si c'est le cas
             >
                 <View style={[styles.iconContainer, activeTab === 'Search' && styles.activeIconContainer]}>
                     <Icon name="search" size={24} color={activeTab === 'Search' ? '#fff' : '#fff'}/>
                 </View>
                 <Text style={[styles.label, activeTab === 'Search' && styles.activeLabel]}>Search</Text>
-            </TouchableOpacity>
+            </AnimatedTouchableOpacity>
         </View>
     );
 };
+
+const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
 
 const styles = StyleSheet.create({
     container: {
